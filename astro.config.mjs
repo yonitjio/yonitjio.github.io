97b1c0250c5cf55ccec97d53dff6c153a593da00
Yoni <yoni.tjio@gmail.com>
2025-04-19 21:34:37 +0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeNova from 'starlight-theme-nova';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://yonitjio.github.io',
+	integrations: [
+		starlight({
+			title: '',
+			logo: {
+				dark: './src/assets/logo-dark.svg',
+				light: './src/assets/logo-light.svg',
+				replacesTitle: true,
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/yonitjio' }],
+			sidebar: [
+				{
+					label: 'Exploring Odoo',
+					autogenerate: { directory: 'exploring-odoo' },
+				}
+			],
+			plugins: [starlightThemeNova()],
+		}),
+	],
+});
---
+++ src/content/docs/exploring-odoo.md
@@ -0,0 +1,3 @@
+---
+title: Exploring Odoo
+---
